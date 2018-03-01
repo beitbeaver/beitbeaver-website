74caf4c0921ac637d1b467c9da6a726ba0ff6c33
Beit Beaver <beitbeaver@gmail.com>
2018-03-01 02:06:07 +0000

    add skyscraper and banner ad elements

--- _plugins/ads.rb
+++ _plugins/ads.rb
@@ -1,8 +1,6 @@
 module Jekyll
 
-  $caption_font_size_percentage = 60
-  $caption_text = "From The Beit Beaver's sponsors"
-
+  $caption_text = "From the Beit Beaver's sponsors"
   $img_default_alt_text = "An advert from our sponsors"
 
   class AdFullBannerTag < Liquid::Tag
@@ -14,13 +12,85 @@ module Jekyll
 
     def render(context)
       <<-MARKUP.strip
-      <div>
-        <img src="assets/images/ads/full-banner.gif" alt="#{$img_default_alt_text}">
-        <p style="font-size: #{$caption_font_size_percentage}%;">#{$caption_text}</p>
+      <div class="ad-box">
+        <img src="/assets/images/ads/full-banner.gif" alt="#{$img_default_alt_text}">
+        <p class="ad-caption">#{$caption_text}</p>
+      </div>
+      MARKUP
+    end # render
+  end # class
+
+  class AdLeaderboardTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end # initialize
+
+    def render(context)
+      <<-MARKUP.strip
+      <div class="ad-box">
+        <img src="/assets/images/ads/leaderboard.gif" alt="#{$img_default_alt_text}">
+        <p class="ad-caption">#{$caption_text}</p>
+      </div>
+      MARKUP
+    end # render
+  end # class
+
+  class AdSquareTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end # initialize
+
+    def render(context)
+      <<-MARKUP.strip
+      <div class="ad-box">
+        <img src="/assets/images/ads/square.gif" alt="#{$img_default_alt_text}">
+        <p class="ad-caption">#{$caption_text}</p>
+      </div>
+      MARKUP
+    end # render
+  end # class
+
+  class AdSkyscraperTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end # initialize
+
+    def render(context)
+      <<-MARKUP.strip
+      <div class="ad-box">
+        <img src="/assets/images/ads/skyscraper.gif" alt="#{$img_default_alt_text}">
+        <p class="ad-caption">#{$caption_text}</p>
+      </div>
+      MARKUP
+    end # render
+  end # class
+
+  class AdMiniSkyscraperTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end # initialize
+
+    def render(context)
+      <<-MARKUP.strip
+      <div class="ad-box">
+        <img src="/assets/images/ads/mini-skyscraper.gif" alt="#{$img_default_alt_text}">
+        <p class="ad-caption">#{$caption_text}</p>
       </div>
       MARKUP
     end # render
   end # class
 end # module
 
-Liquid::Template.register_tag('adfullbanner', Jekyll::AdFullBannerTag)
+Liquid::Template.register_tag('ad_fullbanner', Jekyll::AdFullBannerTag)
+Liquid::Template.register_tag('ad_leaderboard', Jekyll::AdLeaderboardTag)
+Liquid::Template.register_tag('ad_square', Jekyll::AdSquareTag)
+Liquid::Template.register_tag('ad_skyscraper', Jekyll::AdSkyscraperTag)
+Liquid::Template.register_tag('ad_mini_skyscraper', Jekyll::AdMiniSkyscraperTag)
